c98815575dfa3fb44a7aacee9c7ddc27e5f8f331
Michael Gross <96743493+Doom-137@users.noreply.github.com>
2022-01-06 13:49:12 -0500

    Update Set-SequentialFileNames.ps1

--- file_handling/Set-SequentialFileNames.ps1
+++ file_handling/Set-SequentialFileNames.ps1
@@ -1,4 +1,4 @@
-${__Title} = 'Mass File Rename_Replace with Sequential Numbers'
+${__Title} = 'Batch File Rename_Replace with Sequential Numbers'
 ${__Description} = @"
 Asks for initial file path, loops through files 
 in (sub)directories and renames files with seque-
@@ -7,31 +7,26 @@ mbering resets with every individual directory.
 "@
 
 #################################################
-
 $Counter = 0
-$TargetDirectory = Read-Host 'Enter file path'
-$Padding = Read-Host "Enter desired file name length (or type 'n' for auto-padding)"
-Set-Location $TargetDirectory
-$SubDirectories = $(Get-ChildItem $TargetDirectory)
+$TargetDir = Read-Host 'Enter file path:'
+$Padding = Read-Host "Enter desired file name length:"
+Set-Location $TargetDir
+$SubDirs = $(Get-ChildItem $TargetDir)
 
-ForEach($SubDirectory in $SubDirectories) {
-	$Files = $(Get-ChildItem $SubDirectory)
-    #Reset counter in preparation for next subdirectory
+ForEach($SubDir in $SubDirs) {
+	$Files = $(Get-ChildItem $SubDir)
 	$Counter = 0
 
 	ForEach($File in $Files) {
 		$FileName = $File.FullName
-		#Get current file extension
 		$FileExtension = [System.IO.Path]::GetExtension($File)
 		If ($Padding -EQ 'n') {
 			$NewNameWorkspace = $Counter.ToString().PadLeft((($Files.Count.ToString()).Length) -replace ' ','0')
 		} Else {
-			#Pad [Counter] for [NewFileName]
 			$NewNameWorkspace = $Counter.ToString().PadLeft([int]$Padding,'0')
 		}
 		$NewFileName = $NewNameWorkspace + $FileExtension
 		Rename-Item $FileName $NewFileName
-		#Add 1 to [Counter]
 		$Counter += 1
 	}
 }
