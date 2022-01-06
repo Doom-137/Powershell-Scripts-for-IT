88d38a3878b16d6329d61273100d9c7d7890ab4b
Michael Gross <96743493+Doom-137@users.noreply.github.com>
2022-01-06 13:50:12 -0500

    Update Set-RandomFileNames.ps1

--- file_handling/Set-RandomFileNames.ps1
+++ file_handling/Set-RandomFileNames.ps1
@@ -1,4 +1,4 @@
-${__Title} = 'Mass File Rename_Replace with Random Characters'
+${__Title} = 'Batch File Rename_Replace with Random Characters'
 ${__Description} = @"
 Asks for initial file path, loops through files 
 in (sub)directories and renames files with rando-
@@ -6,19 +6,17 @@ m characters regardless of file extension.
 "@
 
 #################################################
-$TargetDirectory = Read-Host 'Enter file path'
-$RandomStringLength = Read-Host 'Enter desired file name length'
-Set-Location $TargetDirectory
-$SubDirectories = $(Get-ChildItem $TargetDirectory)
+$TargetDir = Read-Host 'Enter file path:'
+$RandomStringLength = Read-Host 'Enter desired file name length:'
+Set-Location $TargetDir
+$SubDirs = $(Get-ChildItem $TargetDir)
 
-ForEach($SubDirectory in $SubDirectories) {
-	$Files = $(Get-ChildItem $SubDirectory)
+ForEach($SubDir in $SubDirs) {
+	$Files = $(Get-ChildItem $SubDir)
 
 	ForEach($File in $Files) {
 		$FileName = $File.FullName
-		#Get current file extension
 		$FileExtension = [System.IO.Path]::GetExtension($File)
-		#Pad [Counter] for [NewFileName]
 		$NewNameWorkspace = -Join ((33..126) * [int]$RandomStringLength | Get-Random -Count 32 | % {[char]$_})
 		$NewFileName = $NewNameWorkspace + $FileExtension
 		Rename-Item $FileName $NewFileName
